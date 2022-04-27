ea14e19f8c1dabcdcb4890b33c06399701764019
AlisaK3 <73519510+AlisaK3@users.noreply.github.com>
2022-04-27 14:16:35 +0300

    something changed here...

---
+++ Source/Splanc_demo/MyClass.h
@@ -0,0 +1,15 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+/**
+ * 
+ */
+class SPLANC_DEMO_API MyClass
+{
+public:
+	MyClass();
+	~MyClass();
+};
